73ace841b03a2603c5bf4a917923efc2acb9065f
gustavo <gustavo.isaac@gmail.com>
2022-02-14 18:50:21 +0000

    GsonParser & JsonParser

---
+++ app/src/main/java/com/artemissoftware/dictionaryapp/feature_dictionary/data/local/WordInfoDatabase.kt
@@ -0,0 +1,16 @@
+package com.artemissoftware.dictionaryapp.feature_dictionary.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.artemissoftware.dictionaryapp.feature_dictionary.data.local.entities.WordInfoEntity
+
+@Database(
+    entities = [WordInfoEntity::class],
+    version = 1
+)
+//--@TypeConverters(Converters::class)
+abstract class WordInfoDatabase: RoomDatabase() {
+
+    abstract val dao: WordInfoDao
+}
