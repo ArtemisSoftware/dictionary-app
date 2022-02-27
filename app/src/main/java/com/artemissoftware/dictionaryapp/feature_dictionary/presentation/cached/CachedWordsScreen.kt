ae7dcb419bf851cb4c261146b583047c919ab3c2
gustavo <gustavo.isaac@gmail.com>
2022-02-27 13:56:09 +0000

    CachedWordsScreen adjustment

--- app/src/main/java/com/artemissoftware/dictionaryapp/feature_dictionary/presentation/cached/CachedWordsScreen.kt
+++ app/src/main/java/com/artemissoftware/dictionaryapp/feature_dictionary/presentation/cached/CachedWordsScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -26,12 +27,17 @@ fun CachedWordsScreen(){
                 .background(color = MaterialTheme.colors.background)
         ) {
 
-            LazyColumn {
-                items(items = state.wordInfoItems) { word ->
-                    WordItem(wordInfo = word)
-                    Divider(modifier = Modifier.padding(horizontal = 16.dp))
+            Column {
+                Title()
+
+                LazyColumn {
+                    items(items = state.wordInfoItems) { word ->
+                        WordItem(wordInfo = word)
+                        Divider(modifier = Modifier.padding(horizontal = 16.dp))
+                    }
                 }
             }
+
 
             if(state.isLoading) {
                 CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
@@ -41,38 +47,53 @@ fun CachedWordsScreen(){
 }
 
 @Composable
+private fun Title(){
+
+    Column(
+        modifier = Modifier
+            .padding(all = 16.dp)
+            .fillMaxWidth()
+    ) {
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = "Cached Words",
+            style = MaterialTheme.typography.h5,
+            fontWeight = FontWeight.Bold,
+            maxLines = 1
+        )
+
+        Divider(
+            modifier = Modifier.padding(vertical = 8.dp),
+            color = Color.Black
+        )
+    }
+}
+
+@Composable
 private fun WordItem(
     wordInfo: WordInfo
 ) {
-//    Surface(
-//        modifier = Modifier
-//            .fillMaxWidth(),
-//        color = MaterialTheme.colors.taskItemBackgroundColor,
-//        shape = RectangleShape,
-//        elevation = TASK_ITEM_ELEVATION,
-//        onClick = {
-//            navigateToTaskScreen(toDoTask.id)
-//        }
-//    ) {
-        Column(
-            modifier = Modifier
-                .padding(all = 16.dp)
-                .fillMaxWidth()
-        ) {
-            Text(
-                modifier = Modifier.fillMaxWidth(),
-                text = wordInfo.word,
-                style = MaterialTheme.typography.h5,
-                fontWeight = FontWeight.Bold,
-                maxLines = 1
-            )
 
-            Text(
-                modifier = Modifier.fillMaxWidth(),
-                text = wordInfo.phonetic,
-                style = MaterialTheme.typography.subtitle1,
 
-            )
-        }
-//    }
+    Column(
+        modifier = Modifier
+            .padding(all = 16.dp)
+            .fillMaxWidth()
+    ) {
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = wordInfo.word,
+            style = MaterialTheme.typography.h5,
+            fontWeight = FontWeight.Bold,
+            maxLines = 1
+        )
+
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = wordInfo.phonetic,
+            style = MaterialTheme.typography.subtitle1,
+
+        )
+    }
+
 }
